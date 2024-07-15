40aaa44941ee9c55168c7c4316bc2588187f3991
haoliang <haoliang0.1.2@gmail.com>
2024-07-15 12:48:45 +0800

    initial commit

---
+++ lua/textexchange.lua
@@ -0,0 +1,109 @@
+local itertools = require("infra.itertools")
+local jelly = require("infra.jellyfish")("textexchange", "debug")
+local ni = require("infra.ni")
+local vsel = require("infra.vsel")
+local wincursor = require("infra.wincursor")
+
+local xmark_ns = ni.create_namespace("textexchange.extmarks")
+
+local xmarks = {}
+do
+  ---@param bufnr integer
+  ---@param range infra.vsel.Range
+  ---@return integer xmid
+  function xmarks.set(bufnr, range, hi)
+    return ni.buf_set_extmark(bufnr, xmark_ns, range.start_line, range.start_col, {
+      end_row = range.stop_line - 1,
+      end_col = range.stop_col,
+      hl_group = hi,
+      right_gravity = false,
+      end_right_gravity = true,
+    })
+  end
+
+  ---@param bufnr integer
+  ---@param xmid integer
+  ---@return infra.vsel.Range?
+  function xmarks.range(bufnr, xmid)
+    local xm = ni.buf_get_extmark_by_id(bufnr, xmark_ns, xmid, { details = true })
+    if #xm == 0 then return end
+
+    return {
+      start_line = xm[1],
+      start_col = xm[2],
+      stop_line = xm[3].end_row + 1, --0-based; exclusive
+      stop_col = xm[3].end_col, --0-based; exclusive
+    }
+  end
+end
+
+---@param bufnr integer
+---@param range infra.vsel.Range
+---@return string[]
+local function text_from_range(bufnr, range) --
+  return ni.buf_get_text(bufnr, range.start_line, range.start_col, range.stop_line - 1, range.stop_col, {})
+end
+
+---@param bufnr integer
+---@param range infra.vsel.Range
+---@param text string[]
+local function range_become_text(bufnr, range, text) --
+  ni.buf_set_text(bufnr, range.start_line, range.start_col, range.stop_line - 1, range.stop_col, text)
+end
+
+---@param src {bufnr:integer,xmid:integer}
+---@param dest {bufnr:integer,xmid:integer}
+local function exchange(src, dest)
+  if not ni.buf_is_valid(src.bufnr) then return jelly.warn("src bufnr is invalid") end
+
+  local src_range = xmarks.range(src.bufnr, src.xmid)
+  if src_range == nil then return jelly.warn("src xmark is gone") end
+  ni.buf_del_extmark(src.bufnr, xmark_ns, src.xmid)
+
+  local src_text = text_from_range(src.bufnr, src_range)
+  local dest_text = text_from_range(dest.bufnr, assert(xmarks.range(dest.bufnr, dest.xmid)))
+  if itertools.equals(src_text, dest_text) then return jelly.info("src and dest are the same") end
+
+  range_become_text(src.bufnr, src_range, dest_text)
+
+  --dest range may change after src text changed
+  local dest_range = xmarks.range(dest.bufnr, dest.xmid)
+  if dest_range == nil then return jelly.warn("dest xmark is gone") end
+  ni.buf_del_extmark(dest.bufnr, xmark_ns, dest.xmid)
+
+  range_become_text(dest.bufnr, dest_range, src_text)
+
+  --stylua: ignore start
+  jelly.info(
+    "exchanged src=#%d;%d:%d;%d:%d dest=#%d;%d:%d;%d:%d",
+    src.bufnr, src_range.start_line, src_range.start_col, src_range.stop_line, src_range.stop_col,
+    dest.bufnr, dest_range.start_line, dest_range.start_col, dest_range.stop_line, dest_range.stop_col
+  )
+  --stylua: ignore end
+end
+
+local state = {
+  src = nil, ---@type nil|{bufnr:integer, xmid:integer}
+}
+
+return function()
+  local winid = ni.get_current_win()
+  local cursor = wincursor.last_position(winid)
+  local bufnr = ni.win_get_buf(winid)
+  local range = vsel.range(bufnr, true)
+  if range == nil then return jelly.info("no selecting range") end
+
+  if state.src == nil then
+    local xmid = xmarks.set(bufnr, range, "Search")
+    state.src = { bufnr = bufnr, xmid = xmid }
+    jelly.info("source marked")
+  else
+    local xmid = xmarks.set(bufnr, range)
+    local dest = { bufnr = bufnr, xmid = xmid }
+    exchange(state.src, dest)
+    state.src = nil
+  end
+
+  --keep the cursor where it was: https://github.com/tommcdo/vim-exchange/issues/56
+  wincursor.go(winid, cursor.lnum, cursor.col)
+end
---
+++ readme.md
@@ -0,0 +1,20 @@
+to exchange text between two places handily
+
+## design choices, features, limits
+* work with utf-8 text
+* work in visual mode `v` and `V`, but not `ctrl-V`
+* work across buffers
+
+## prerequisites
+* nvim 0.10.*
+* haolian9/infra.nvim
+
+## usage
+my personal config
+```
+m.x("X", ":lua require'textexchange'()<cr>")
+cmds.create("Exchange", function() require("textexchange")() end, { nargs = 0, range = true })
+```
+
+## credits
+* awesome [vim-exchange](https://github.com/tommcdo/vim-exchange) which i had been using til nvim 0.7
